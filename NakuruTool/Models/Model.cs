ff4ee851a4031ecf2f9131b7245fc622f894d38e
rice <rice5941@gmail.com>
2025-08-02 14:17:14 +0900

    Livet .net6.0

---
+++ NakuruTool/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Livet;
+
+namespace NakuruTool.Models
+{
+    public class Model : NotificationObject
+    {
+    }
+}
